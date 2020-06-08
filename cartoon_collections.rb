8f7bcbaa7cdf5b50d5ac528c198e4ed42c9e7e42
Nathan Dorazil <nathand1915@gmail.com>
2020-06-08 01:52:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,7 @@ def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if (snacks.include?(cheese))
+      return cheese
+    end
   end
 end
